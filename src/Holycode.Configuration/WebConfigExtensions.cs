255db58b4903a203f833ee5ad399e00be24c0dde
piotr.kryger <piotr.kryger@legimi.com>
2022-03-11 10:14:24 +0100

    more package fixes

--- src/Holycode.Configuration/WebConfigExtensions.cs
+++ src/Holycode.Configuration/WebConfigExtensions.cs
@@ -5,7 +5,8 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.Extensions.Configuration;
+
+using ConfigManager = System.Configuration.ConfigurationManager;
 
 namespace Microsoft.Extensions.Configuration
 {
@@ -18,19 +19,20 @@ namespace Microsoft.Extensions.Configuration
         /// <returns></returns>
         public static IConfigurationBuilder AddWebConfig(this IConfigurationBuilder configuration)
         {
-            var keys = ConfigurationManager.AppSettings.AllKeys;
+            
+            var keys = ConfigManager.AppSettings.AllKeys;
             var dict = new Dictionary<string, string>();
 
             foreach (var k in keys)
             {
-                var val = ConfigurationManager.AppSettings[k];
+                var val = ConfigManager.AppSettings[k];
                 dict[k.Replace(".", ":")] = val;
                 var check = configuration.Get(k.Replace(".", ":"));
             }
 
 
 
-            var connectionStrings = ConfigurationManager.ConnectionStrings;
+            var connectionStrings = ConfigManager.ConnectionStrings;
             foreach (ConnectionStringSettings c in connectionStrings)
             {
                 dict[$"connectionStrings:{c.Name}:connectionString"] = c.ConnectionString;
@@ -43,19 +45,19 @@ namespace Microsoft.Extensions.Configuration
 
         public static IConfiguration AddWebConfig(this IConfiguration configuration)
         {
-            var keys = ConfigurationManager.AppSettings.AllKeys;
+            var keys = ConfigManager.AppSettings.AllKeys;
             var dict = new Dictionary<string, string>();
 
             foreach (var k in keys)
             {
-                var val = ConfigurationManager.AppSettings[k];
+                var val = ConfigManager.AppSettings[k];
                 configuration.Set(k.Replace(".", ":"), val);
                 //var check = configuration.Get(k.Replace(".", ":"));
             }
 
 
 
-            var connectionStrings = ConfigurationManager.ConnectionStrings;
+            var connectionStrings = ConfigManager.ConnectionStrings;
             foreach (ConnectionStringSettings c in connectionStrings)
             {
                 configuration.Set($"connectionStrings:{c.Name}:connectionString",c.ConnectionString);
@@ -71,7 +73,7 @@ namespace Microsoft.Extensions.Configuration
             return configuration;
         }
 
-        public static IConfiguration FillAppSettings(this IConfiguration configuration) => FillAppSettings(configuration, ConfigurationManager.AppSettings);
+        public static IConfiguration FillAppSettings(this IConfiguration configuration) => FillAppSettings(configuration, ConfigManager.AppSettings);
         public static IConfiguration FillAppSettings(this IConfiguration configuration, System.Collections.Specialized.NameValueCollection appSettings)
         {
             configuration.Traverse((key, val) => { appSettings[key.Replace(':', '.')] = val; });
